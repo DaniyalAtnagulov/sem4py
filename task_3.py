93a5d32d94f18d2c1f2e59a331a4cb582d8b4716
Daniyal~ <danialastrachan@gmail.comgit config --global user.email danialastrachan@gmail.com>
2023-01-11 21:03:26 +0300

    task_3.py

---
+++ task_3.py
@@ -0,0 +1,10 @@
+import numpy
+from random import randint as r
+ 
+list_inp = [r(-10, 30) for i in range(20)]
+
+res = numpy.array(list_inp)
+unique_res = numpy.unique(res)
+print(res)
+print()
+print(" Unique elements of the list:\n",unique_res)
